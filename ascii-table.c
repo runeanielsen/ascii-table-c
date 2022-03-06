0a82a4c21e907ae51a784349bcb2a5362f562034
Rune Nielsen <runenielsen@runbox.com>
2022-03-06 10:17:16 +0100

    changed whitespace chars to be less misleading

--- ascii-table.c
+++ ascii-table.c
@@ -27,9 +27,7 @@ void pad_left(char* s, size_t l) {
 }
 
 char get_char(size_t i) {
-  if (i <= 31) {
-    return '-';
-  } else if (i == 32 || i == 127) {
+  if (i < 33 || i == 127) {
     return ' ';
   } else {
     return (char)i;
