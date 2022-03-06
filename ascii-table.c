118fce8efdaca99e5a3b8fd24e21c5d910bda9b7
Rune Nielsen <runenielsen@runbox.com>
2022-03-06 15:47:47 +0100

    to make ocd people happy

--- ascii-table.c
+++ ascii-table.c
@@ -96,7 +96,7 @@ int main() {
     }
   }
 
-  for (size_t i = 0; i <= 31; i++) {
+  for (size_t i = 0; i < 32; i++) {
     char* row = create_ascii_row(i);
     printf("%s\n", row);
     free(row);
