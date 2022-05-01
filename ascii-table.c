3e80d94d440eeefd275178bb5943f387888dac9b
Rune Nielsen <runenielsen@runbox.com>
2022-05-01 11:23:34 +0200

    correct formatting

--- ascii-table.c
+++ ascii-table.c
@@ -1,8 +1,8 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-void pad_left(char* s, size_t l) {
+void pad_left(char *s, size_t l) {
   /* If there is no difference, do not pad. */
   if (l == strlen(s)) {
     return;
@@ -26,45 +26,41 @@ void pad_left(char* s, size_t l) {
   strncpy(s, padding, l + 1);
 }
 
-char get_char(size_t i) {
-  return i < 33 || i == 127
-    ? ' '
-    : (char)i;
-}
+char get_char(size_t i) { return i < 33 || i == 127 ? ' ' : (char)i; }
 
-char* get_dec(size_t i) {
+char *get_dec(size_t i) {
   size_t needed = snprintf(NULL, 0, "%zu", i);
-  char* s = malloc(needed + 1);
+  char *s = malloc(needed + 1);
   sprintf(s, "%zu", i);
   return s;
 }
 
-char* get_hex(unsigned int i) {
+char *get_hex(unsigned int i) {
   size_t needed = snprintf(NULL, 0, "%x", i);
-  char* s = malloc(needed + 1);
+  char *s = malloc(needed + 1);
   sprintf(s, "%x", i);
   return s;
 }
 
-char* get_octal(unsigned int i) {
+char *get_octal(unsigned int i) {
   size_t needed = snprintf(NULL, 0, "%d", i);
-  char* s = malloc(needed + 1);
+  char *s = malloc(needed + 1);
   sprintf(s, "%o", i);
   return s;
 }
 
-char* create_ascii_row(size_t i) {
-  char* s = calloc(73, sizeof(char));
+char *create_ascii_row(size_t i) {
+  char *s = calloc(73, sizeof(char));
 
   for (size_t j = 0; j < 4; j++) {
     size_t tmp_i = i + (32 * j);
 
     char c = get_char(tmp_i);
-    char* dec = get_dec(tmp_i);
+    char *dec = get_dec(tmp_i);
     pad_left(dec, 4);
-    char* hex = get_hex(tmp_i);
+    char *hex = get_hex(tmp_i);
     pad_left(hex, 4);
-    char* oct = get_octal(tmp_i);
+    char *oct = get_octal(tmp_i);
     pad_left(oct, 4);
 
     char tmp_s[16];
@@ -95,7 +91,7 @@ int main() {
   }
 
   for (size_t i = 0; i < 32; i++) {
-    char* row = create_ascii_row(i);
+    char *row = create_ascii_row(i);
     printf("%s\n", row);
     free(row);
   }
