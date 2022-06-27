48f6eda9c209262b74ad1ce7ed3aa9e8b8c691cf
Rune Nielsen <runenielsen@runbox.com>
2022-06-27 10:00:59 +0200

    simplified formatting

--- ascii-table.c
+++ ascii-table.c
@@ -8,16 +8,10 @@ char get_char(size_t i) {
 
 void create_ascii_row(size_t i, char* buffer, size_t max_length) {
   for (size_t j = 0; j < 4; j++) {
-    unsigned int cell = i + (32 * j);
-
-    char tmp_s[32] = "";
-    if (j == 3) {
-      snprintf(tmp_s, sizeof(tmp_s), "%3u %4x %4o %2c", cell, cell, cell, get_char(cell));
-    } else {
-      snprintf(tmp_s, sizeof(tmp_s), "%3u %4x %4o %2c | ", cell, cell, cell, get_char(cell));
-    }
-
-    strncat(buffer, tmp_s, max_length - 1);
+    unsigned int bv = i + 32 * j;
+    char row_block[32] = "";
+    snprintf(row_block, sizeof(row_block), "%3u %4x %4o %2c | ", bv, bv, bv, get_char(bv));
+    strncat(buffer, row_block, max_length - 1);
   }
 }
 
@@ -34,11 +28,8 @@ void create_ascii_body(char* buffer, size_t max_size) {
 void create_ascii_header(char* buffer, size_t max_size) {
   for (size_t i = 0; i < 4; i++) {
     strncat(buffer, "Dec  Hex  Oct  C", max_size - 1);
-    if (i < 3) {
-      strncat(buffer, " | ", max_size - 1);
-    }
+    strncat(buffer, " | ", max_size - 1);
   }
-
   strncat(buffer, "\n", max_size - 1);
 }
 
@@ -51,6 +42,5 @@ int main() {
   char ascii_table[4096];
   create_ascii_table(ascii_table, sizeof(ascii_table));
   printf("%s", ascii_table);
-
   return EXIT_SUCCESS;
 }
