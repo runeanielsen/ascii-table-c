0c80445cdaf30b030c8bf03e0b88e8ab370a87e7
Rune Nielsen <runenielsen@runbox.com>
2022-06-27 10:16:56 +0200

    improves naming

--- ascii-table.c
+++ ascii-table.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-char get_char(size_t i) { return i < 33 || i == 127 ? ' ' : i; }
+char char_formatting(size_t i) { return i < 33 || i == 127 ? ' ' : i; }
 
-void create_ascii_row(size_t i, char *buffer, size_t max_length) {
+void ascii_row(size_t i, char *buffer, size_t max_length) {
   for (size_t j = 0; j < 4; j++) {
     unsigned int bv = i + 32 * j;
     char block[32] = "";
-    snprintf(block, sizeof(block), "%3u %4x %4o %2c", bv, bv, bv, get_char(bv));
+    snprintf(block, sizeof(block), "%3u %4x %4o %2c", bv, bv, bv, char_formatting(bv));
     strncat(buffer, block, max_length - 1);
     if (j < 3) {
       strncat(buffer, " | ", max_length - 1);
@@ -16,17 +16,17 @@ void create_ascii_row(size_t i, char *buffer, size_t max_length) {
   }
 }
 
-void create_ascii_body(char *buffer, size_t max_size) {
+void ascii_body(char *buffer, size_t max_size) {
   char row[128] = "";
   for (size_t i = 0; i < 32; i++) {
-    create_ascii_row(i, row, sizeof(row));
+    ascii_row(i, row, sizeof(row));
     strncat(buffer, row, max_size - 1);
     strncat(buffer, "\n", max_size - 1);
     memset(row, 0, sizeof(row));
   }
 }
 
-void create_ascii_header(char *buffer, size_t max_size) {
+void ascii_header(char *buffer, size_t max_size) {
   for (size_t i = 0; i < 4; i++) {
     strncat(buffer, "Dec  Hex  Oct  C", max_size - 1);
     if (i < 3) {
@@ -36,14 +36,14 @@ void create_ascii_header(char *buffer, size_t max_size) {
   strncat(buffer, "\n", max_size - 1);
 }
 
-void create_ascii_table(char *buffer, size_t max_size) {
-  create_ascii_header(buffer, max_size);
-  create_ascii_body(buffer, max_size);
+void ascii_table(char *buffer, size_t max_size) {
+  ascii_header(buffer, max_size);
+  ascii_body(buffer, max_size);
 }
 
 int main() {
-  char ascii_table[4096];
-  create_ascii_table(ascii_table, sizeof(ascii_table));
-  printf("%s", ascii_table);
+  char ascii_table_buf[4096];
+  ascii_table(ascii_table_buf, sizeof(ascii_table_buf));
+  printf("%s", ascii_table_buf);
   return EXIT_SUCCESS;
 }
